5ec9fdbdcbd6c682b13c9aece65a9941efc6c645
Anas Deis <anas.deis@mail.mcgill.ca>
2018-03-05 11:07:17 -0500

    Added missing operations for moveTable in RestoAppController

---
+++ src/ca/mcgill/ecse223/resto/controller/RestoAppController.java
@@ -0,0 +1,196 @@
+package ca.mcgill.ecse223.resto.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.mcgill.ecse223.resto.application.RestoApplication;
+import ca.mcgill.ecse223.resto.model.Menu;
+import ca.mcgill.ecse223.resto.model.MenuItem;
+import ca.mcgill.ecse223.resto.model.MenuItem.ItemCategory;
+import ca.mcgill.ecse223.resto.model.RestoApp;
+import ca.mcgill.ecse223.resto.model.Seat;
+import ca.mcgill.ecse223.resto.model.Table;
+
+<<<<<<< HEAD
+import static ca.mcgill.ecse223.resto.application.RestoApplication.save;
+
+=======
+>>>>>>> 8cb507198007e33bf0e5064d146d53699f664cbd
+public class RestoAppController {
+
+	public static List<Table> getTables(){
+		return RestoApplication.getRestoApp().getCurrentTables();
+	}
+	public static void createTable(int numberOfSeats, int x, int y, int width, int length) throws InvalidInputException {	
+		RestoApp ra = RestoApplication.getRestoApp();
+		int generatedTableNumber = RestoAppController.generateTableNumber();
+		
+		String error = "";
+		if (numberOfSeats <= 0) {
+			error = "The number of seats must be greater than 0. ";
+		}
+		if (x < 0) {
+			error = error + "The x location must be non-negative. ";
+		}
+		if (y < 0) {
+			error = error + "The y location must be non-negative. ";
+		}
+		if (width <= 0) {
+			error = error + "The width must be non-negative. ";
+		}
+		if (length <= 0) {
+			error = error + "The location must be non-negative. ";
+		}
+		if (isDuplicateTableNumber(generatedTableNumber)){
+			error = error + "Table with table number " + generatedTableNumber + " already exists. ";
+		}
+		if(isTableOverlapping(x,y,width,length)){
+			error = error + "Table will overlap with another table. ";
+		}
+		if (error.length() > 0) {
+			throw new InvalidInputException(error.trim());
+		}
+		
+		try {
+			Table table = new Table(generatedTableNumber, x, y, width, length, ra);
+			for(int i = 0; i < numberOfSeats; i++){
+				table.addCurrentSeat(new Seat(table));
+			}
+			ra.addCurrentTable(table);
+<<<<<<< HEAD
+			save();
+=======
+			RestoApplication.save();
+>>>>>>> 8cb507198007e33bf0e5064d146d53699f664cbd
+		}
+		catch (Exception e) {
+			error = e.getMessage();
+			throw new InvalidInputException(e.getMessage());
+		}
+	}
+	public static void removeTable(Integer selectedTableNumber) throws InvalidInputException {
+		RestoApp ra = RestoApplication.getRestoApp();
+		String error = "";
+
+		try {
+			for (Table table : ra.getCurrentTables()) {
+				if(selectedTableNumber.equals(table.getNumber())) {
+					ra.removeCurrentTable(table);
+					break;
+				}
+			}
+<<<<<<< HEAD
+			save();
+=======
+			RestoApplication.save();
+>>>>>>> 8cb507198007e33bf0e5064d146d53699f664cbd
+		}
+		catch (Exception e) {
+			error = e.getMessage();
+			throw new InvalidInputException(e.getMessage());
+		}
+		
+	}
+
+	private static boolean isDuplicateTableNumber(int number) {
+		RestoApp ra = RestoApplication.getRestoApp();
+		List<Table> tables = ra.getCurrentTables();
+		System.out.println(tables.size());
+		for(int i = 0; i< tables.size(); i++){
+			if(tables.get(i).getNumber() == number){
+				return true;
+			}
+		}
+		return false;
+	}
+	public static int generateTableNumber() {
+		RestoApp ra = RestoApplication.getRestoApp();
+		List<Table> tables = ra.getCurrentTables();
+		if(tables.size() == 0)return 1;
+		else return tables.get(tables.size()-1).getNumber() + 1;
+
+	}
+	
+	// Checks if the table that you are trying to input overlaps with another
+	// It starts counting at index [0][0] for x and y.
+	public static boolean isTableOverlapping(int x, int y, int width, int length) {
+		RestoApp ra = RestoApplication.getRestoApp();
+		List<Table> tables = ra.getCurrentTables();
+		int[][] tableMap = new int[9999][9999];
+		
+		for(int k = 0; k < tables.size(); k++){
+			for(int i = 0; i< tables.get(k).getWidth(); i++){
+				for(int j = 0; j < tables.get(k).getLength(); j++){
+					tableMap[tables.get(k).getX() + i][tables.get(k).getY() + j] = 1;
+				}
+			}
+		}
+		for(int i = 0; i< width; i++){
+			for(int j = 0; j < length; j++){
+				if(tableMap[i + x][j + y] == 1){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public static ArrayList<MenuItem> getMenuItem(ItemCategory itemCategory) throws InvalidInputException {
+
+		if (itemCategory.equals(null)) {
+			throw new InvalidInputException("Item category is null"); 
+		}
+		
+		RestoApp resto = RestoApplication.getRestoApp();
+		ArrayList<MenuItem> itemList = new ArrayList<MenuItem>();
+		Menu menu = resto.getMenu();
+		for (int j = 0; j < menu.numberOfMenuItems(); j++) {
+			MenuItem currItem = menu.getMenuItem(j);
+			if (currItem.getItemCategory().equals(itemCategory)) {
+				itemList.add(currItem);
+			}
+		}
+		
+		return itemList; 
+	}
+
+	public static void moveTable(Table table, int x, int y) throws InvalidInputException {
+
+		String error = "";
+		if(table == null){
+			error += "Table cannot be null ";
+		}
+		if(x < 0){
+			error += "x cannot be negative ";
+		}
+		if(y < 0){
+			error += "y cannot be negative ";
+		}
+		if (error.length() > 0){
+			throw (new InvalidInputException(error.trim()));
+		}
+
+		int width = table.getWidth();
+		int length = table.getLength();
+		RestoApp r = RestoApplication.getRestoApp();
+		List<Table> currentTables = r.getCurrentTables();
+
+		try {
+
+			for (Table currentTable : currentTables) {
+				if ((isTableOverlapping(x, y, width, length) == true) && (table.equals(currentTable) == false)) {
+					throw (new InvalidInputException("Tables overlap! Change table configuration."));
+				}
+			}
+		} catch (InvalidInputException e) {
+			System.out.println(e.getMessage());
+		}
+<<<<<<< HEAD
+
+		table.setX(x);
+		table.setY(y);
+		RestoApplication.save();
+=======
+>>>>>>> 8cb507198007e33bf0e5064d146d53699f664cbd
+	}
+}
---
+++ src/ca/mcgill/ecse223/resto/test/RestoApplicationTest.java
@@ -0,0 +1,66 @@
+package ca.mcgill.ecse223.resto.test;
+
+
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import ca.mcgill.ecse223.resto.application.RestoApplication;
+import ca.mcgill.ecse223.resto.controller.InvalidInputException;
+import ca.mcgill.ecse223.resto.controller.RestoAppController;
+import ca.mcgill.ecse223.resto.model.Menu;
+import ca.mcgill.ecse223.resto.model.MenuItem;
+import ca.mcgill.ecse223.resto.model.RestoApp;
+import ca.mcgill.ecse223.resto.model.MenuItem.ItemCategory;
+import ca.mcgill.ecse223.resto.model.PricedMenuItem;
+
+public class RestoApplicationTest {
+	RestoApp resto;
+	
+	@Before
+	public void setUp() {
+		
+		resto = RestoApplication.getRestoApp();
+	}
+	
+	@Test
+	public void getMenuItemTest() {
+	Menu menu = resto.getMenu();
+	MenuItem firstItem = menu.addMenuItem("KONG PAO Chicken");
+	firstItem.setItemCategory(ItemCategory.Main);
+	PricedMenuItem priceOfFirstItem = new PricedMenuItem(15,resto,firstItem);
+	firstItem.setCurrentPricedMenuItem(priceOfFirstItem);
+	
+	MenuItem secondItem = menu.addMenuItem("Seven Up");
+	firstItem.setItemCategory(ItemCategory.NonAlcoholicBeverage);
+	PricedMenuItem priceOfSecondItem = new PricedMenuItem(3,resto,secondItem);
+	secondItem.setCurrentPricedMenuItem(priceOfSecondItem);
+	
+	try {
+		ArrayList<MenuItem> itemList1 = RestoAppController.getMenuItem(ItemCategory.Main);
+		assertEquals(itemList1.size(),1);
+		assertEquals(itemList1.get(0).getName(),"KONG BAO Chicken");
+		RestoApplication.save();
+	} catch (InvalidInputException e) {
+		
+	}
+	
+	try {
+		ArrayList<MenuItem> itemList2 = RestoAppController.getMenuItem(ItemCategory.NonAlcoholicBeverage);
+		assertEquals(itemList2.size(),1);
+		assertEquals(itemList2.get(0).getName(),"Seven Up");
+		RestoApplication.save();
+	} catch (InvalidInputException e) {
+		
+	}
+	try {
+		ArrayList<MenuItem> itemList3 = RestoAppController.getMenuItem(null);	
+	} catch (InvalidInputException e) {
+		assertEquals(e.getMessage(),"Item category is null");
+	}
+	}
+	
+}
