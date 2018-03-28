66620a22aa7d6b7cad072d1bd684f5c85006cfb7
aaronhhjang <heehyun.jang@mail.mcgill.ca>
2018-03-27 20:35:19 -0400

    Removed unnecessary comments

--- src/ca/mcgill/ecse223/resto/view/MenuDisplay.java
+++ src/ca/mcgill/ecse223/resto/view/MenuDisplay.java
@@ -1,6 +1,5 @@
 package ca.mcgill.ecse223.resto.view;
 
-import java.awt.Frame;
 import java.util.ArrayList;
 
 import javax.swing.JDialog;
@@ -21,30 +20,23 @@ public class MenuDisplay extends JFrame {
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setSize(1000, 600);
 
-        //getContentPane().setLayout(null);
-
         ArrayList<MenuItem> menuItems = null;
-        //JLabel lbl = null;
+  
         try {
             if (category.equals("Appetizer")) {
                 menuItems = RestoAppController.getMenuItem(MenuItem.ItemCategory.Appetizer);
-                //lbl = new JLabel("Appetizer");
                 setTitle("Appetizer");
             } else if (category.equals("Main")) {
                 menuItems = RestoAppController.getMenuItem(MenuItem.ItemCategory.Main);
-                //lbl = new JLabel("Main");
                 setTitle("Main");
             } else if (category.equals("Dessert")) {
                 menuItems = RestoAppController.getMenuItem(MenuItem.ItemCategory.Dessert);
-                //lbl = new JLabel("Dessert");
                 setTitle("Dessert");
             } else if (category.equals("Alcoholic Beverage")) {
                 menuItems = RestoAppController.getMenuItem(MenuItem.ItemCategory.AlcoholicBeverage);
-                //lbl = new JLabel("Alcoholic Beverage");
                 setTitle("Alcoholic Beverage");
             } else if (category.equals("Non Alcoholic Beverage")) {
                 menuItems = RestoAppController.getMenuItem(MenuItem.ItemCategory.NonAlcoholicBeverage);
-                //lbl = new JLabel("Non Alcoholic Beverage");
                 setTitle("Non Alcoholic Beverage");
             }
         } catch (InvalidInputException e) {
@@ -56,13 +48,7 @@ public class MenuDisplay extends JFrame {
         }
 
 
-        //lbl.setBounds(12, 13, 54, 32);
-        //this.getContentPane().add(lbl);
-
-////		JPanel apptizerPanel = new JPanel();
-////		apptizerPanel.setBounds(12, 58, 179, 494);
-////		getContentPane().add(apptizerPanel);
-//		
+    
         //TODO Currently displays the "MenuItem" not the fields of it
         JList<Object> itemList = new JList<>(menuItems.toArray());
         JScrollPane menuScrollPane = new JScrollPane(itemList);
@@ -79,60 +65,5 @@ public class MenuDisplay extends JFrame {
     }
 
 	
-	/*
-    private String error;
 
-	public MenuDisplay() {
-		initComponents();
-	}
-
-	private void initComponents() {
-		menuPageFrame = new JFrame("Menu");
-		menuPageFrame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-		menuPageFrame.setResizable(true);
-		menuPageFrame.getContentPane().setLayout(null);
-		
-		JLabel lblAppetizer = new JLabel("Appetizer");
-		lblAppetizer.setBounds(12, 13, 56, 16);
-		menuPageFrame.getContentPane().add(lblAppetizer);
-		
-		JPanel appetizerPanel = new JPanel();
-		appetizerPanel.setBounds(12, 30, 122, 306);
-		menuPageFrame.getContentPane().add(appetizerPanel);
-		
-		ArrayList<MenuItem> menuItems = null;
-		try {
-			menuItems = RestoAppController.getMenuItem(MenuItem.ItemCategory.Appetizer);
-		} catch (InvalidInputException e) {
-			error = e.getMessage();
-			JDialog er = new JDialog(MenuDisplay.menuPageFrame, error, false);
-			er.setSize(325, 20);
-			er.setLocationRelativeTo(MenuDisplay.menuPageFrame);
-			er.setVisible(true);
-		}
-//		JList<String> appetizerList = new JList<>(menuItems.toArray(new String[0]));
-		JList<Object> appetizerList = new JList<>(menuItems.toArray());
-		JScrollPane scrollPane = new JScrollPane(appetizerList);
-		
-		appetizerPanel.add(appetizerList);
-		
-
-		
-		/*
-		ItemCategory[] itemCategories = MenuItem.ItemCategory.values();
-		for (int i = 0; i < itemCategories.length; i++) {
-			try {
-				ArrayList<MenuItem> menuItems = RestoAppController.getMenuItem(itemCategories[i]);
-			} catch (InvalidInputException e) {
-				error = e.getMessage();
-				JDialog er = new JDialog(MenuDisplay.menuPageFrame, error, false);
-				er.setSize(325, 20);
-				er.setLocationRelativeTo(MenuDisplay.menuPageFrame);
-				er.setVisible(true);
-			}
-
-			JList<MenuItem> displayItems = new JList();
-		}
- 	*/
-    //}
 }
