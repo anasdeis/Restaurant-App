e222590f72f5275751344864be2ba00fd1f34271
aaronhhjang <heehyun.jang@mail.mcgill.ca>
2018-04-03 19:27:32 -0400

    Code regeneration

--- src/ca/mcgill/ecse223/resto/model/OrderItem.java
+++ src/ca/mcgill/ecse223/resto/model/OrderItem.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 import java.util.*;
 
 // line 32 "../../../../../RestoAppPersistence.ump"
-// line 79 "../../../../../RestoApp v3.ump"
+// line 81 "../../../../../RestoApp v3.ump"
 public class OrderItem implements Serializable
 {
 
--- src/ca/mcgill/ecse223/resto/model/Waiter.java
+++ src/ca/mcgill/ecse223/resto/model/Waiter.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 import java.util.*;
 
 // line 62 "../../../../../RestoAppPersistence.ump"
-// line 106 "../../../../../RestoApp v3.ump"
+// line 108 "../../../../../RestoApp v3.ump"
 public class Waiter implements Serializable
 {
 
